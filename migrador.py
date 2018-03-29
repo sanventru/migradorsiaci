899dda88497218644fa137c6f9fd829243018862
santiago <sanventru@gmail.com>
2018-03-28 22:44:20 -0500

    mensaje grafico

--- migrador.py
+++ migrador.py
@@ -5,45 +5,34 @@ import requests
 import datetime
 import simplejson as json
 import winreg
-
+from tkinter import *
+from tkinter import messagebox
 
 hKey = winreg.OpenKey(winreg.HKEY_CURRENT_USER,"Software\ODBC\ODBC.INI\spagfic")
 registrov = str(winreg.QueryValueEx(hKey, "CommLinks")).split('=')[1].split('}')[0]
-
-
+registrov = registrov.strip()
 server_rest = 'http://' + registrov + ':8000'
 #server_rest = 'http://186.4.251.26:8000'
 #server_rest = 'http://localhost:8000'
-
 database = 'interface'
 username = 'SAC'
 password = 'SAC'
-
 #database = 'interface' 
 #username = 'sa' 
 #password = 'quinde07'
-
 desde = ''
 hasta = ''
 tipo = ''
-
-parametros = sys.argv # Fecha_inicio Fecha_fin Tipo
+agencia = ''
+parametros = sys.argv
 desde = parametros[1].replace('_', ' ')
 hasta = parametros[2].replace('_', ' ')
 tipo = parametros[3]
-estacion = parametros[4]
-
-#desde = '2018-01-01'
-#hasta = '2019-01-01'
-#tipo = 1
-#estacion = 'san'
-
-
+agencia = parametros[4]
 
 def myconverter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
-
 
 conn = pyodbc.connect('DSN=' + database + ';UID=' + username + ';PWD=' + password)
 cur = conn.cursor()
@@ -54,14 +43,18 @@ header = [i[0] for i in cur.description]
 data = cur.fetchall()
 for i in data:
     d = dict(zip(header, i))
-    d.update({'estacion':estacion})
+    d.update({'agencia':agencia})
     data_json.append(d)
 j = data_json
 j = json.loads(json.dumps(j, use_decimal=True, default = myconverter))
 r = requests.post(server_rest + '/insertpost', json=j)
 if r.status_code == requests.codes.ok:
-    print('si')
+    root = Tk()
+    root.withdraw()
+    messagebox.showinfo("Siaci", "Proceso exitoso")
 else:
-    print('no')
+    root = Tk()
+    root.withdraw()
+    messagebox.showerror("Siaci", "Error en la transferencia")
 cur.close()
 conn.close()
